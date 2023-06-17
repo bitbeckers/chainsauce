8cfd6f905ed98b7b39890948bb4023b331109c3f
bitbeckers <code@bitbeckers.com>
2023-06-17 12:14:18 +0200

    fix(test): ignore dist and nore_modules

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "jest";
 const config: Config = {
   extensionsToTreatAsEsm: [".ts"],
   testEnvironment: "node",
+  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
   transform: {
     // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
     // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
